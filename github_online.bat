5549afb2dc5cd986836d93034d18031f811ef121
blackaquaindia <blackaquaindia@gmail.com>
2024-04-08 02:13:18 +0530

    Local Update

---
+++ github_online.bat
@@ -0,0 +1,7 @@
+cd "/storage/emulated/0/Git/blackaquapydroid/"
+git add .
+git config --global user.email "blackaquaindia@gmail.com"
+git config --global user.name "Abhijeet"
+git commit -m "Local Update"
+git push origin
+git push origin main
